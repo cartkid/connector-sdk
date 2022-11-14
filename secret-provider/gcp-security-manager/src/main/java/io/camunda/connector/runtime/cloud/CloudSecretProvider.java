fce10e1fecb0c2c32459887e8c8df9619927998c
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-11-14 12:51:56 +0100

    file format

--- secret-provider/gcp-security-manager/src/main/java/io/camunda/connector/runtime/cloud/CloudSecretProvider.java
+++ secret-provider/gcp-security-manager/src/main/java/io/camunda/connector/runtime/cloud/CloudSecretProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. Camunda licenses this file to you under the Apache License,
+ * Version 2.0; you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.camunda.connector.runtime.cloud;
 
 import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
@@ -9,7 +25,6 @@ import io.camunda.connector.api.secret.SecretProvider;
 import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,10 +46,12 @@ public class CloudSecretProvider implements SecretProvider {
     Objects.requireNonNull(clusterId, "You need to specify the clusterId to load secrets for");
     LOGGER.info("Fetching secrets for cluster {} from secret manager", clusterId);
     try (final SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
-      final String projectId = Objects.requireNonNull(
+      final String projectId =
+          Objects.requireNonNull(
               System.getenv(SECRETS_PROJECT_ENV_NAME),
               "Environment variable " + SECRETS_PROJECT_ENV_NAME + " is missing");
-      final String secretPrefix = Objects.requireNonNull(
+      final String secretPrefix =
+          Objects.requireNonNull(
               System.getenv(SECRETS_PREFIX_ENV_NAME),
               "Environment variable " + SECRETS_PREFIX_ENV_NAME + " is missing");
 
